3a1dd643268e714427fa011cfe9542bc4be3cb09
clauvillar <elportalcreativok@gmail.com>
2022-05-10 15:01:36 -0500

    primer push proyecto final sena-censa

---
+++ nosotros.php
@@ -0,0 +1,20 @@
+<?php include("templates/header.php"); ?>
+
+<div class="featurette" id="nosotros">
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h1 class="featurette-heading">Nosotros</h1> <h2><span> <br><br>Estoy haciendo prueba de edicion en php</span></h1>
+        <p class="lead"> <br>Somos una empresa de desarrollo que ofrece un sistema de información de productos químicos, brindando la información necesaria
+                        para atender las necesidades de nuestros clientes, además de cumplir con la normatividad legal vigente referente a sustancias químicas en el pais,
+                        protegiendo la salud de sus trabajadores, el medio ambiente y la infraestructura.
+
+                        La mayoría de matrices o herramientas de valoración de riesgos solo tienen en cuenta uno de estos aspectos.
+                         </p>
+      </div>
+         <div class="col-md-5">
+             <img src="img/hero3.png" width="500" height="500"></img>
+        </div>
+    </div>
+</div>
+
+<?php include("templates/footer.php"); ?>
---
+++ templates/footer.php
@@ -0,0 +1,49 @@
+</div>
+</div>
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="footer-contact">
+                            <h3>Nuestras oficinas</h3>
+                            <p><i class="fa fa-map-marker-alt"></i>Cra 70 #55-25 Medellin, Colombia</p>
+                            <p><i class="fa fa-phone-alt"></i>+57 604-8962514</p>
+                            <p><i class="fa fa-envelope"></i>softawechemical@gmail.com</p>
+                        </div>
+                    </div>
+                    <div class="col-md-5">                               
+                        <div class="footer-social">
+                            <h3>Nuestras Redes Sociales </h3>
+                            <div>                                  
+                            <a href=""><i class="fab fa-twitter"></i></a>
+                            <a href=""><i class="fab fa-facebook-f"></i></a>
+                            <a href=""><i class="fab fa-youtube"></i></a>
+                            <a href=""><i class="fab fa-instagram"></i></a>
+                            <a href=""><i class="fab fa-linkedin-in"></i></a>
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="footer-newsletter">
+                    <h3>¿Qué Ofrecemos?</h3>
+                    <p>
+                    Software que suplan las necesidades de las empresas con el control de los inventarios de los productos quimicos almacenados en cada una de ellas.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container copyright">
+        <p>copyright @ <a href="https://anacastanod.com">anacastanod</a></p>
+    </div>
+</div>
+<!-- Footer End -->
+</body>
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</html>
